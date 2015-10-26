2b6ac7cc4cee78c2f827a55024cb08dfb4007979
Juliana <jsayumi@gmail.com>
2015-10-26 11:30:31 -0700

    clear text input

--- app/src/main/java/com/example/guest/tasklist/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/tasklist/ui/MainActivity.java
@@ -62,5 +62,6 @@ public class MainActivity extends ListActivity {
        category.save();
        mCategories.add(name);
        mAdapter.notifyDataSetChanged();
+       mNewCategoryText.setText("");
    }
 }
