e31bb3997e1a4532a12d93fa2c73e630aeabddde
Juliana <jsayumi@gmail.com>
2015-10-26 10:56:29 -0700

    add categories

---
+++ app/src/main/java/com/example/guest/tasklist/models/Category.java
@@ -0,0 +1,51 @@
+package com.example.guest.tasklist.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+import java.util.List;
+
+/**
+ * Created by Guest on 10/26/15.
+ */
+@Table(name = "Categories", id = "_id")
+
+public class Category extends Model {
+    @Column(name = "name")
+    public String mName;
+
+    public Category() {
+        super();
+    }
+
+    public Category(String name) {
+        super();
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public static List<Category> all() {
+        return new Select().from(Category.class).execute();
+    }
+    
+    public List<Task> tasks() {
+        return getMany(Task.class, "Category");
+    }
+
+    public static Category find(String name){
+        return new Select()
+                .from(Category.class)
+                .where("Name = ?", name)
+                .executeSingle();
+    }
+
+}
---
+++ app/src/main/java/com/example/guest/tasklist/models/Task.java
@@ -0,0 +1,39 @@
+package com.example.guest.tasklist.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+import java.util.List;
+
+@Table(name = "Tasks", id = "_id")
+public class Task extends Model {
+
+    @Column(name = "Description")
+    private String mDescription;
+    @Column(name= "Category")
+    private Category mCategory;
+
+    public Task() {
+        super();
+    }
+
+    public Task(String description, Category category) {
+        super();
+        mDescription = description;
+        mCategory = category;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public static List<Task> all() {
+        return new Select().from(Task.class).execute();
+    }
+}
